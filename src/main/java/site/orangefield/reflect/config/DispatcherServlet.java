991a7f9785c32faa123cc197089a5ca4abcba53d
field <jsw_777@naver.com>
2022-06-20 14:32:43 +0900

    메서드 실행하기 - 모든 메서드가 실행됨

--- src/main/java/site/orangefield/reflect/config/DispatcherServlet.java
+++ src/main/java/site/orangefield/reflect/config/DispatcherServlet.java
@@ -1,6 +1,7 @@
 package site.orangefield.reflect.config;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,15 +13,15 @@ import site.orangefield.reflect.util.UtilsLog;
 import site.orangefield.reflect.web.MemberController;
 
 @WebServlet("/")
-public class DispatcherServlet extends HttpServlet{
-	
+public class DispatcherServlet extends HttpServlet {
+
 	public DispatcherServlet() {
-	
+
 	}
 
 	private static final long serialVersionUID = 1L; // 지금은 몰라도 됨. 그냥 걸어두셈
 	private static final String TAG = "DispatcherServlet : ";
-	
+
 	@Override
 	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		UtilsLog.getInstance().info(TAG, "doDelete()");
@@ -28,25 +29,27 @@ public class DispatcherServlet extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		MemberController controller = new MemberController();
+		MemberController memberController = new MemberController();
 
-		
 		UtilsLog.getInstance().info(TAG, "doGet()");
 		UtilsLog.getInstance().info(TAG, req.getRequestURI());
-		
+
 		String identifier = req.getRequestURI();
-		
-		if(identifier.equals("/join")) {
+
+		// 리플렉션 발동 /login
+		Method[] methods = memberController.getClass().getDeclaredMethods();
+		for (Method method : methods) {
+			UtilsLog.getInstance().info(TAG, method.getName());
+			String idf = identifier.replace("/", "");
+			if (idf.equals(method.getName())) {
+				UtilsLog.getInstance().info(TAG, idf + " 메서드를 실행합니다");
 				try {
-					controller.join(req, resp);
+					method.invoke(memberController, req, resp);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-		} else if(identifier.equals("/login")) {		
-			controller.login(req, resp);			
-		} else if(identifier.equals("/findById")) {		
-			controller.findById(req, resp);			
-		}		
+			}
+		}
 	}
 
 	@Override
